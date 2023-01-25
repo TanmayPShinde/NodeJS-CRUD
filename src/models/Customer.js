fd183f0840e1173ae37b9b117304f691675b3ae4
TanmayPShinde <tps.shinde@gmail.com>
2023-01-25 17:52:36 +0530

    connection string database name note

--- src/models/Customer.js
+++ src/models/Customer.js
@@ -6,3 +6,6 @@ const customerSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Customer", customerSchema);
+// if we need to change the database in which our collection is getting stored,
+// we need to name it in the connection string in env between slash and ? after mongodb.net
+// BY DEFAULT it goes into test if nothing is given there
