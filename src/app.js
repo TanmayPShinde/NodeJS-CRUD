6cb48de0f99d007003e4f304ac63eed7eead1e1f
TanmayPShinde <tps.shinde@gmail.com>
2023-01-28 12:34:23 +0530

    post request basic and saving data in DB

--- src/app.js
+++ src/app.js
@@ -7,29 +7,40 @@ mongoose.set("strictQuery", false);
 dotenv.config();
 
 const app = express();
+app.use(express.json()); // this is to parse request bodies for post (body-parser)
 const PORT = process.env.PORT || 3000;
 const CONN = process.env.CONN;
 
-const customer = new Customer({
-  name: "Tanmay",
-  industry: "IT",
-});
-
-customer.save();
-
 app.get("/", (req, res) => {
-  res.send(customer);
+  res.send("Welcome to Noob House");
 });
 
-app.post("/", (req, res) => {
-  res.send("This is a post request!");
+app.get("/api/customers", async (req, res) => {
+  console.log(await mongoose.connection.db.listCollections().toArray());
+  try {
+    const result = await Customer.find();
+    res.send({ customers: result });
+  } catch (e) {
+    res.status(500).send({ error: e.message });
+  }
+});
+
+app.post("/api/customers", (req, res) => {
+  console.log(req.body);
+  const customer = new Customer(req.body);
+  try {
+    customer.save();
+    res.status(201).send({ customer });
+  } catch (e) {
+    res.status(400).send({ error: e.message });
+  }
 });
 
 const start = async () => {
   try {
     await mongoose.connect(CONN);
 
-    app.listen(PORT, "127.0.0.1", () => {
+    app.listen(PORT, () => {
       console.log(`Listening on port ${PORT}`);
     });
   } catch (e) {
--- src/models/Customer.js
+++ src/models/Customer.js
@@ -5,7 +5,7 @@ const customerSchema = new mongoose.Schema({
   industry: String,
 });
 
-module.exports = mongoose.model("Customer", customerSchema);
+module.exports = mongoose.model("Customers", customerSchema);
 // if we need to change the database in which our collection is getting stored,
 // we need to name it in the connection string in env between slash and ? after mongodb.net
 // BY DEFAULT it goes into test if nothing is given there
